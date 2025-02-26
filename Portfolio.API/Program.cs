a62681adad4222d647e9549138c96966ad78b50b
Ganesh <ganeshpodishetti@gmail.com>
2025-02-25 22:28:23 -0500

    feat: integrate scalar api reference (scalar-config)

--- Portfolio.API/Program.cs
+++ Portfolio.API/Program.cs
@@ -1,13 +1,13 @@
+using Portfolio.API.Extensions;
+using Portfolio.Domain.Entities;
 using Portfolio.Infrastructure.Extension;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.AddPresentation();
 
 var app = builder.Build();
 
@@ -15,7 +15,25 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("Restaurant API")
+            .WithTheme(ScalarTheme.Mars)
+            .WithDarkMode(true)
+            .WithSidebar(true)
+            .WithDefaultHttpClient(ScalarTarget.Http, ScalarClient.Http11)
+            .Authentication = new ScalarAuthenticationOptions
+            {
+                PreferredSecurityScheme = "Bearer"
+            };
+    });
 }
+
+// app.UseAuthentication(); invoked by calling MapIdentityApi<User>
+// app.MapGroup("api/identity")
+//     .WithTags("Identity")
+//     .MapIdentityApi<User>();
 
 app.UseHttpsRedirection();
 
